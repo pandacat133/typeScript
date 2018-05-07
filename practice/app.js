2deb617817272574dc0b1ccff7565b570811b121
Celie Baker <celie.n133@gmail.com>
2018-05-07 08:36:53 -0600

    generics

--- practice/app.js
+++ practice/app.js
@@ -1,3 +1,4 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+exports.__esModule = true;
 var mag = { issueNumber: 1234 };
+var book = { title: 'A Great Title' };
--- practice/app.ts
+++ practice/app.ts
@@ -1,3 +1,4 @@
-import {Magazine} from "./magazine";
+import {Book, Magazine} from "./magazine";
 
 let mag: Magazine = {issueNumber: 1234};
+let book: Book = {title: 'A Great Title'};
